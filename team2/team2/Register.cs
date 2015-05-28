2638827afd0df41a1e43cafda42f44b8e199cd95
David <davidpeng0216@gmail.com>
2015-05-28 22:44:41 +0800

    Add CheckAcountExist function

--- team2/team2/Register.cs
+++ team2/team2/Register.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace team2
 {
     class Register
     {
+        static internal bool CheckAcountExist(string accountToCheck)
+        {
+            bool accountExist = false;
 
+            char[] delimiters = new char[] { '\t', ' ' };
+            StreamReader sr = new StreamReader("account.txt");
+            while (!sr.EndOfStream) // 每次讀取一行，直到檔尾            
+            {
+                string line = sr.ReadLine();    // 讀取文字到 line 變數
+                string[] item = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                if (item[0].Equals(accountToCheck))
+                {
+                    accountExist = true;
+                    break;
+                }
+            }
+            sr.Close();
+            return accountExist;
+        }
     }
 }
--- team2/team2/UnitTest1.cs
+++ team2/team2/UnitTest1.cs
@@ -7,8 +7,15 @@ namespace team2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void checkAccountExist()
         {
+            Assert.IsTrue(Register.CheckAcountExist("test1"));
+            Assert.IsTrue(Register.CheckAcountExist("test2"));
+            Assert.IsTrue(Register.CheckAcountExist("test3"));
+
+            Assert.IsFalse(Register.CheckAcountExist("test"));
+            Assert.IsFalse(Register.CheckAcountExist("DDD"));
+
         }
     }
 }
