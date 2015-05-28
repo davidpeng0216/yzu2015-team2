8279cd0e09cca64cffcf95b9c4924fb5de4effda
Ensemble3866 <sunny9113@gmail.com>
2015-05-28 21:57:04 +0800

    驗證帳號跟驗證密碼完成

--- team2/team2/OnlineForum.cs
+++ team2/team2/OnlineForum.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace team2
 {
-    class OnlineForum
+    class  OnlineForum
     {
+        static internal bool VerifyAccount(string account)
+        {
+            if (Regex.IsMatch(account,"^[a-zA-Z][a-zA-Z0-9]{7,15}$"))
+                return true;
+            else return false;
+        }
+
+        static internal bool VerifyPassword(string password)
+        {
+            if (Regex.IsMatch(password, "^[a-zA-Z0-9]{8,16}$"))
+                return true;
+            else return false;
+        }
     }
 }
--- team2/team2/UnitTest1.cs
+++ team2/team2/UnitTest1.cs
@@ -4,11 +4,34 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace team2
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestClass
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_1003314()
         {
+            //正常Case
+            Assert.IsTrue(OnlineForum.VerifyAccount("abcde123"));
+            //過長
+            Assert.IsFalse(OnlineForum.VerifyAccount("abcdefghASijk123dkfasfdjasjdkaslkdasld456789"));
+            //過短
+            Assert.IsFalse(OnlineForum.VerifyAccount("abc123"));
+            //非英文開頭
+            Assert.IsFalse(OnlineForum.VerifyAccount("123abc"));
+            //包含英文和數字以外的字元
+            Assert.IsFalse(OnlineForum.VerifyAccount("123我是中文"));
+            //正常Case
+            Assert.IsTrue(OnlineForum.VerifyPassword("123456789"));
+            Assert.IsTrue(OnlineForum.VerifyPassword("abc12345"));
+            Assert.IsTrue(OnlineForum.VerifyPassword("125abcde"));
+            //過長
+            Assert.IsFalse(OnlineForum.VerifyPassword("abcdefghASijk123dkfasfdjasjdkaslkdasld456789"));
+            //過短
+            Assert.IsFalse(OnlineForum.VerifyPassword("abc123"));
+            //非英文開頭
+            Assert.IsFalse(OnlineForum.VerifyPassword("123abc"));
+            //包含英文和數字以外的字元
+            Assert.IsFalse(OnlineForum.VerifyPassword("123我是中文"));
+            
         }
     }
 }
